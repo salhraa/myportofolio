6e13dff8a9cff0d92c16bf3ee1fa7be8b693af96
Salhraa <salhrnii@gmail.com>
2026-09-21 11:12:21 +0700

    Update: bagian experience extends base.html

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import get_skills_json, show_main, show_experience, show_skills, create_skill, delete_skill
+from main.views import get_skills_json, show_main, show_experience, show_skills, create_skill, delete_skill, show_education, create_education, update_education, delete_education, get_education_json
 
 
 app_name = "main"
@@ -13,4 +13,9 @@ urlpatterns = [
     path("skills/", show_skills, name="show_skills"),
     path("api/skills/", get_skills_json, name="get_skills_json"),
     path("skills/<uuid:skill_id>/delete/", delete_skill, name="delete_skill"),
+    path("education/", show_education, name="show_education"),
+    path("education/add/", create_education, name="create_education"),
+    path("education/<uuid:education_id>/edit/", update_education, name="update_education"),
+    path("education/<uuid:education_id>/delete/", delete_education, name="delete_education"),
+    path("api/education/", get_education_json, name="get_education_json"),
 ]
